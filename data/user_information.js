4439fb1669a65cc5de1d1c3ed9f6e20fef0292d8
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-07-07 13:18:29 +0300

    user data

---
+++ data/user_information.js
@@ -0,0 +1,22 @@
+const accounts = [
+  {
+    iban: "trxx yyyy xxxx yyyy",
+    balance: 100,
+  },
+  {
+    iban: "trxx yyyy xxxx yyyx",
+    balance: 5040,
+  },
+  {
+    iban: "trxx yyyy xxxx xyyx",
+    balance: 10594,
+  },
+];
+
+const user = {
+  name: "Jane",
+  surname: "Doe",
+  accounts,
+};
+
+export default user;
