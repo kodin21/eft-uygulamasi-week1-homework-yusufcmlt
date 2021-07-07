9e710fec186298250b8ac983bbeff8ad6234010d
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-07-07 13:17:54 +0300

    form ici degiskenlerin tanimi

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import formData from "./data/form-data";
+import user from "./data/user_information";
 import {
   clearInputValues,
   getRandomTheme,
@@ -9,22 +9,24 @@ import {
   transactionCompleted,
 } from "./helper/helper-functions";
 
-//Form State Verisi
-const formState = {};
+const formState = {}; //Form iceriginin state verisi.
 
-//Form elemaninin tanimlanmasi
-const formElement = document.querySelector("form");
+const highLimit = 500; //Belirlenebilecek kontrol limiti, sonrasinda sifre sormasi gerekir.
+const timeLimit = 400; // Belirlenebilecek zaman limiti (saniye), timer icerisinde kullanilir.
+const passwordAttemptLimit = 3; // Belirlenebilecek password girme sayisi.
 
-//Form gonderme durumu ve kontroller
+const formElement = document.querySelector("form"); //Form elemaninin secilmesi.
+
+//Formun gonderilme durumu ve kontroller.
 formElement.addEventListener("submit", handleFormSubmit);
 
 function handleFormSubmit(event) {
   event.preventDefault();
 
   //Gonderilecek miktarin 500 birimden fazla olmasi.
-  if (formState[inputAmount.name] > 500) {
+  if (formState[inputAmount.name] >= highLimit) {
     //Sifre girebilme hakkiyla beraber sifre giris alani
-    higherAmountTransaction(3);
+    higherAmountTransaction(passwordAttemptLimit);
   } else {
     transactionCompleted();
   }
@@ -32,28 +34,56 @@ function handleFormSubmit(event) {
 
 //Eleman-event olusturucu fonksiyonlar
 inputEventCreator(formElement, handleInputChange);
-timerCreator(formElement);
-selectGroupCreator(formData, formState, handleInputChange);
+timerCreator(formElement, timeLimit);
+selectGroupCreator(user.accounts, formState, handleInputChange);
 
 //Form ici elemanlarin tanimlanmasi.
 const selectElement = formElement.querySelector("select");
 const [inputIBAN, inputAmount] = formElement.querySelectorAll("input");
 const buttonElement = formElement.querySelector("button");
 
-//Input change eventi
-//Form icerisinde input ve select elemanlari degistiginde form kontrol edilir
-//formState verisi input degerlerine gore guncellenir
-//Para girisi input elemaninin max sayi degeri guncellenir
+/*
+ * Input change eventi
+ * Form icerisinde input ve select elemanlari degistiginde form kontrol edilir
+ * formState verisi input degerlerine gore guncellenir
+ * Para girisi input elemaninin max sayi degeri guncellenir
+ */
 function handleInputChange(event) {
-  const { name, value } = event.target;
+  let { name, value, type } = event.target;
   formState[name] = value;
+
+  if (type === "text") {
+    event.target.value = value.trim(); //Bos karakter kullaniminin engellenmesi
+    event.target.value = splitInput(value); //Karakterlerin 4lu gruplar halinde bolunmesi
+    console.log(event.target.validity.valid);
+    event.target.validity.valid = false;
+    console.log(event.target.validity.valid);
+  }
 
   setInputMaxAttribute(formState[selectElement.name]);
   checkFormValidity();
 }
 
-//HTML5 form kontrolleri yardimiyla formun valid olup olmadiginin boolean donmesi.
-//Form validity durumunun kontrolu ve butona etkisi
+function splitInput(text) {
+  text = text.split("-").join("");
+  let modifiedText = "";
+
+  //Regex ile yapilabilirdi//
+  //Inputa girilen iban degerinin 4 karakterde bir "-" ile bolunmesi
+  for (let i = 0; i < text.length; i++) {
+    modifiedText += text[i];
+    if ((i + 1) % 4 == 0 && i + 1 !== text.length) {
+      modifiedText += "-";
+    }
+  }
+
+  //console.log(modifiedText);
+  return modifiedText;
+}
+/**
+ * HTML5 form kontrolleri yardimiyla formun valid olup olmadiginin boolean donmesi.
+ * Form validity durumunun kontrolu ve butona etkisi
+ */
 function checkFormValidity() {
   const isFormValid = formElement.checkValidity();
   toggleButtonAccess(isFormValid);
@@ -69,7 +99,6 @@ function toggleButtonAccess(buttonState) {
 
 //Dropdown menudeki bakiyenin para miktari inputundaki max girilebilecek sayiyi guncellemesi
 function setInputMaxAttribute(maxAmountValue) {
-  console.log(maxAmountValue);
   inputAmount.setAttribute("max", `${maxAmountValue}`);
 }
 
